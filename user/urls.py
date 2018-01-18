abb553c268337fef2ccb4909c1b61256c38c7217
장지용 <jangjiyong@jangjiyong-ui-MacBook-Pro.local>
2018-01-18 16:45:06 +0900

    logout url name 설정

--- user/urls.py
+++ user/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     url(r'^login/$',auth_views.login, {"template_name":"user/login.html"}, name="login"),
-    url(r'^logout/$',auth_views.logout, {'next_page':'/'}),
+    url(r'^logout/$',auth_views.logout, {'next_page':'/'}, name="logout"),
 ]
