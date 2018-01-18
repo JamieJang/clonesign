c476a5627d811b75b8b7846d71c9c64eac1d0195
장지용 <jangjiyong@jangjiyong-ui-MacBook-Pro.local>
2018-01-18 14:25:30 +0900

    user logout

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -7,7 +8,9 @@ app_name = "user"
 
 urlpatterns = [
     url(r'^$',RedirectView.as_view(url="/signup")),
-    url(r'^signup/',views.signup.as_view(),name="signup"),
+    url(r'^signup/$',views.signup.as_view(),name="signup"),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
+    
+    url(r'^logout/$',auth_views.logout, {'next_page':'/'})
 ]
