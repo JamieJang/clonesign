6e9fe6c2d3e288539566a6671ea2323c17be54f6
장지용 <jangjiyong@jangjiyong-ui-MacBook-Pro.local>
2018-01-18 19:48:08 +0900

    create Document model

--- document/admin.py
+++ document/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+@admin.register(models.Document)
+class AdminDocument(admin.ModelAdmin):
+    list_display = ('id','creator','docs','status','created_at','updated_at')
+    list_display_links = ('id', 'creator', 'docs')
+    search_fields = ('creator','docs','status')
+    list_filter = ('status','created_at','updated_at','creator')
--- document/models.py
+++ document/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from user import models as user_models
+from datetime import datetime
 
-# Create your models here.
+class Timestamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+    
+class Folder(Timestamp):
+    name = models.CharField(max_length=140)
+
+    def __str__(self):
+        return self.name
+
+class Document(Timestamp):
+    def generate_filename(self,filename):
+        today = datetime.today()
+        return "document/{}/{}/{}/{}".format(self.user.username, today.year, today.month, today.day)
+
+    STATUS_LIST = (
+        ('서명 전', '서명 전'),
+        ('내 서명 필요','내 서명 필요'),
+        ('상대 서명 필요','상대 서명 필요'),
+        ('완료','완료'),
+        ('거절','거절'),
+        ('취소','취소'),
+    )
+
+    creator = models.ForeignKey(user_models.MyUser, on_delete=models.CASCADE, related_name="docs_myself")
+    docs = models.FileField(upload_to=generate_filename)
+    parters = models.ManyToManyField(user_models.MyUser,related_name="docs_by_partners")
+    status = models.CharField(max_length=50, choices=STATUS_LIST)
+    folder = models.ForeignKey(Folder,on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return "{} / {} / {}".format(self.docs, self.status, self.creator)
