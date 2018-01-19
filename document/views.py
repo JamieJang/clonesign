8abf0bb29883161573c19394ef67acf1379496fd
장지용 <jangjiyong@jangjiyong-ui-MacBook-Pro.local>
2018-01-19 15:26:44 +0900

    create docs list output page

--- document/views.py
+++ document/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
 
+from . import models
+
 class index(View):
     def get(self,request):
         if not request.user.is_authenticated:
             return redirect("/")
+        
+        user = request.user
+        self_docs = user.self_docs_count
+        part_docs = user.part_docs_count
+        total_docs = self_docs + part_docs
+        docs = models.Document.objects.filter(creator=user)
 
-        return render(request, 'document/index.html')
+
+        return render(request, 'document/index.html',{'total_docs':total_docs,
+                                                        'self_docs':self_docs,
+                                                        'part_docs':part_docs,
+                                                        'docs':docs})
