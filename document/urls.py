82b6b95a4139d1de5f1d70a5b35a66b971049a34
장지용 <jangjiyong@jangjiyong-ui-MacBook-Pro.local>
2018-01-19 23:41:51 +0900

    show list by self, partners

--- document/urls.py
+++ document/urls.py
@@ -6,5 +6,7 @@ app_name = "document"
 
 urlpatterns =[ 
     url(r'^$',views.index.as_view(), name="docu-index"),
-    url(r'^upload/$',views.UploadDocs.as_view(),name="upload-docs")
+    url(r'^upload/$',views.UploadDocs.as_view(),name="upload-docs"),
+    url(r'^self/$',views.selfDocs.as_view(),name="self-docs"),
+    url(r'^by_partner/$', views.docsByPartner.as_view(), name="docs-by-partner"),
 ]
--- document/views.py
+++ document/views.py
@@ -15,8 +15,8 @@ class index(View):
         self_docs = user.self_docs_count
         part_docs = user.part_docs_count
         total_docs = self_docs + part_docs
-        docs = models.Document.objects.filter(creator=user)
-
+        docs = list(models.Document.objects.filter(creator=user))
+        docs += list(user.docs_by_partners.all())
 
         return render(request, 'document/index.html',{'total_docs':total_docs,
                                                         'self_docs':self_docs,
@@ -42,3 +42,36 @@ class UploadDocs(View):
         new_docs.save()
 
         return redirect("document:docu-index")
+
+
+class selfDocs(View):
+    def get(self,request):
+        if not request.user.is_authenticated:
+            return redirect("/")
+
+        user = request.user
+        self_docs = user.self_docs_count
+        part_docs = user.part_docs_count
+        total_docs = self_docs + part_docs
+        docs = models.Document.objects.filter(creator=user)
+
+        return render(request, 'document/index.html', {'total_docs': total_docs,
+                                                       'self_docs': self_docs,
+                                                       'part_docs': part_docs,
+                                                       'docs': docs})
+
+class docsByPartner(View):
+    def get(self,request):
+        if not request.user.is_authenticated:
+            return redirect("/")
+
+        user = request.user
+        self_docs = user.self_docs_count
+        part_docs = user.part_docs_count
+        total_docs = self_docs + part_docs
+        docs = user.docs_by_partners.all()
+
+        return render(request, 'document/index.html',{'total_docs':total_docs,
+                                                        'self_docs':self_docs,
+                                                        'part_docs':part_docs,
+                                                        'docs':docs})
